dc3263f814fdcf4e173c83b1b1de7e4931b83306
kayeeesan <eijansantosdominique13@gmail.com>
2025-12-16 23:44:57 +0800

    auth and user added

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        if ($this->isMethod('post')) {
+            return [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|string|min:8|confirmed',
+            ];
+        } else {
+            return [
+                'name' => 'required|string|max:255',
+                'email' => [
+                    'required',
+                    'email',
+                    Rule::unique('users', 'email')->ignore($this->route('uuid'), 'uuid'),
+                ],
+                'password' => 'nullable|string|min:8|confirmed',
+            ];
+        }
+    }
+}
