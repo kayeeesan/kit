b210ccc81bbbbc96fc141b7804fdbeac47684979
kayeeesan <eijansantosdominique13@gmail.com>
2025-12-17 22:41:02 +0800

    user added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <!-- <div id="app"></div> -->
-     <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, distinctio. Tenetur possimus distinctio animi a obcaecati ut deleniti itaque nemo exercitationem molestiae placeat cumque numquam, amet et incidunt nobis culpa!</h1>
+     
 </body>
 </html>
